95baffc26e5e5599dd91aead305b0a9a4565f8b7
EricJamesCrow <EricCrow@pm.me>
2023-08-19 20:16:39 -0700

    created project api
    - created project api post route
    - connected to frontend

---
+++ src/app/api/project/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/lib/prisma";
+
+interface RequestBody {
+    name: string;
+    description: string;
+    type: string;
+    tech: string[];
+    status: string;
+    image?: string;
+
+}
+
+export async function POST(request: Request) {
+    const body:RequestBody = await request.json();
+
+    const project = await prisma.project.create({
+        data: {
+            name: body.name,
+            description: body.description,
+            type: body.type,
+            tech: body.tech,
+            status: body.status,
+            image: body.image ? body.image : null,
+        }
+    });
+    
+    return new Response(JSON.stringify(project));
+}
--- src/components/admin/addproject.tsx
+++ src/components/admin/addproject.tsx
@@ -1,5 +1,6 @@
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Input, Textarea, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
+import {Modal, ModalContent, ModalHeader, ModalBody, Input, Textarea, Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button} from "@nextui-org/react";
 import ImageDropzone from "../ImageDropzone";
+import { useMemo, useState } from "react";
 
 interface AddProjectProps {
     isOpen: boolean;
@@ -7,6 +8,44 @@ interface AddProjectProps {
 }
 
 const AddProject: React.FC<AddProjectProps> = ({ isOpen, onOpenChange }) => {
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState('');
+    const [description, setDescription] = useState('');
+    const [url, setUrl] = useState('');
+    const [tech, setTech] = useState([]);
+    const [type, setType] = useState('');
+
+    const [selectedKeys, setSelectedKeys] = useState(new Set([]));
+
+    const selectedValue = useMemo(
+      () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
+      [selectedKeys]
+    );
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const res = await fetch('/api/project', {
+            method: "POST",
+            body: JSON.stringify({
+              name,
+              status,
+              description,
+              url,
+              tech,
+              type
+            }),
+            headers: { "Content-Type": "application/json" }
+          })
+
+        if (res.ok) {
+            console.log("Project added!");
+        } else {
+            const data = await res.json();
+            console.log("Error adding project:", data);
+        }
+    }
+
     return (
         <Modal isOpen={isOpen} onOpenChange={onOpenChange} backdrop="blur" className="dark text-foreground">
         <ModalContent>
@@ -14,44 +53,51 @@ const AddProject: React.FC<AddProjectProps> = ({ isOpen, onOpenChange }) => {
             <>
               <ModalHeader className="flex flex-col gap-1">Add Project</ModalHeader>
               <ModalBody>
-              <div className="shadow-[0px 4px 12px rgba(0,0,0,0.1)] overflow-x-auto mb-2">
+              <form onSubmit={handleSubmit} className="shadow-[0px 4px 12px rgba(0,0,0,0.1)] overflow-x-auto mb-6">
                     <div className="rounded-b-lg h-[504px] bg-[#18181B]">
                         <div className="flex flex-col items-center">
                             <div className="flex-1 flex flex-col gap-4 px-6">
                                 <div className="flex gap-4">
                                     <div className="font-semibold flex flex-col gap-1">
                                         <label>Name</label>
-                                        <Input/>
+                                        <Input key="name" value={name} onChange={(e) => setName(e.target.value)}/>
                                     </div>
                                     <div className="font-semibold flex flex-col gap-1">
                                         <label>Status</label>
-                                        <Dropdown>
+                                        <Dropdown className="dark text-foreground">
                                             <DropdownTrigger>
                                                 <Button 
+                                                className="w-[112.49px]"
                                                 variant="bordered" 
                                                 >
-                                                Select Status
+                                                {status ? status : "Select Status"}
                                                 </Button>
                                             </DropdownTrigger>
-                                            <DropdownMenu aria-label="Static Actions">
-                                                <DropdownItem key="inProgress">In-Progress</DropdownItem>
-                                                <DropdownItem key="completed">Completed</DropdownItem>
+                                            <DropdownMenu 
+                                            aria-label="Static Actions" 
+                                            onAction={(key) => setStatus(key as string)}
+                                            selectionMode="single"
+                                            variant="flat"
+                                            selectedKeys={[status]}
+                                            >
+                                                <DropdownItem key="In-progress">In-progress</DropdownItem>
+                                                <DropdownItem key="Completed">Completed</DropdownItem>
                                             </DropdownMenu>
                                         </Dropdown>
                                     </div>
                                 </div>
                                 <div className="font-semibold flex flex-col gap-1">
                                     <label>Description</label>
-                                    <Textarea/>
+                                    <Textarea key="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
                                 </div>
                                 <div className="flex gap-4">
                                     <div className="font-semibold flex flex-col gap-1">
                                         <label>URL</label>
-                                        <Input/>
+                                        <Input key="url" value={url} onChange={(e) => setUrl(e.target.value)}/>
                                     </div>
                                     <div className="font-semibold flex flex-col gap-1">
                                         <label>Tech</label>
-                                        <Dropdown>
+                                        <Dropdown className="dark text-foreground">
                                             <DropdownTrigger>
                                                 <Button 
                                                 variant="bordered" 
@@ -59,7 +105,18 @@ const AddProject: React.FC<AddProjectProps> = ({ isOpen, onOpenChange }) => {
                                                 Choose Tech
                                                 </Button>
                                             </DropdownTrigger>
-                                            <DropdownMenu aria-label="Static Actions">
+                                            <DropdownMenu 
+                                            aria-label="Single selection actions"
+                                            variant="flat"
+                                            closeOnSelect={false}
+                                            disallowEmptySelection
+                                            selectionMode="multiple"
+                                            selectedKeys={selectedKeys}
+                                            onSelectionChange={(keys: any) => {
+                                                setSelectedKeys(new Set(keys));
+                                                setTech(Array.from(keys));
+                                            }}
+                                            >
                                                 <DropdownItem key="react">React</DropdownItem>
                                                 <DropdownItem key="nodejs">Node.js</DropdownItem>
                                                 <DropdownItem key="aws">AWS</DropdownItem>
@@ -71,6 +128,28 @@ const AddProject: React.FC<AddProjectProps> = ({ isOpen, onOpenChange }) => {
                                         </Dropdown>
                                     </div>
                                 </div>
+                                <div className="font-semibold flex flex-col gap-1">
+                                        <label>Type</label>
+                                        <Dropdown className="dark text-foreground">
+                                            <DropdownTrigger>
+                                                <Button 
+                                                variant="bordered" 
+                                                >
+                                                {type ? type : "Select Type"}
+                                                </Button>
+                                            </DropdownTrigger>
+                                            <DropdownMenu 
+                                            aria-label="Static Actions" 
+                                            onAction={(key) => setType(key as string)}
+                                            selectionMode="single"
+                                            variant="flat"
+                                            selectedKeys={[type]}
+                                            >
+                                                <DropdownItem key="Personal">Personal</DropdownItem>
+                                                <DropdownItem key="Work">Work</DropdownItem>
+                                            </DropdownMenu>
+                                        </Dropdown>
+                                    </div>
                             </div>
                             <div className="flex-1 flex flex-col font-semibold px-5 mt-4 gap-2">
                                 <label>Image</label>
@@ -79,10 +158,10 @@ const AddProject: React.FC<AddProjectProps> = ({ isOpen, onOpenChange }) => {
                         </div>
                         <div className="font-semibold text-sm flex gap-4 justify-center mt-[42px]">
                             <button onClick={onClose} className="w-[121px] h-[38px] rounded-md border border-black bg-white text-black hover:bg-gray-200 hover:bg-gray-300 transition-all duration-200">Cancel</button>
-                            <button onClick={onClose} className="w-[121px] h-[38px] rounded-md bg-[#4CAF50] hover:bg-[#388E3C] transition-all duration-200">Save Changes</button>
+                            <button type="submit" className="w-[121px] h-[38px] rounded-md bg-[#4CAF50] hover:bg-[#388E3C] transition-all duration-200">Save Changes</button>
                         </div>
                     </div>
-            </div>
+            </form>
               </ModalBody>
             </>
           )}
