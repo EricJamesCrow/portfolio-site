15e8e51445f975786467617a8b038d6a8ae0ce42
EricJamesCrow <EricCrow@pm.me>
2023-08-24 16:11:46 -0700

    converted to tailwindcss

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -12,7 +12,7 @@ const handler = NextAuth({
           // e.g. domain, username, password, 2FA token, etc.
           // You can pass any HTML attribute to the <input> tag through the object.
           credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            username: { label: "Username", type: "text" },
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,10 +3,6 @@ import { signIn } from "next-auth/react";
 import React, { useRef, useState, useEffect } from "react";
 import { useRouter } from 'next/navigation';
 
-// styles
-import styles from "./login.module.css";
-
-
 interface IProps {
     searchParams?: { [key: string]: string | string[] | undefined }
 }
@@ -46,20 +42,21 @@ const LoginPage = ({searchParams}: IProps) => {
     };
 
     return (
-        <div className={styles.background}>
-        <form onSubmit={onSubmit} className={styles.loginContainer}> 
-            <div className={styles.inputContainer}>
-                <label htmlFor="userName">Username</label>
-                <input className={styles.inputChakra} type="text" id="userName" onChange={(e) => (userName.current = e.target.value)} />
-            </div>
-            <div className={styles.inputContainer}>
-                <label htmlFor="password">Password</label>
-                <input className={styles.inputChakra} type="password" id="password" onChange={(e) => (password.current = e.target.value)} />
-            </div>
-            <button type="submit" disabled={loading} className={styles.styledButton}>{loading ? <div className={styles.spinner}></div> : "Login"}</button>
-        </form>
-        {error && <p className={styles.error}>{error}</p>}
+        <div className="bg-custom-color min-h-[70vh] flex flex-col items-center">
+            <form onSubmit={onSubmit} className="mt-24 text-white font-medium text-xl w-72 flex flex-col items-center">
+                <div className="mt-6 flex flex-col w-full">
+                    <label htmlFor="userName">Username</label>
+                    <input className="mt-2 w-full px-2.5 py-1.5 text-base leading-6 text-gray-700 bg-gray-100 border border-gray-300 rounded focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none" type="text" id="userName" onChange={(e) => (userName.current = e.target.value)} />
+                </div>
+                <div className="mt-6 flex flex-col w-full">
+                    <label htmlFor="password">Password</label>
+                    <input className="mt-2 w-full px-2.5 py-1.5 text-base leading-6 text-gray-700 bg-gray-100 border border-gray-300 rounded focus:border-blue-500 focus:ring-2 focus:ring-blue-200 outline-none" type="password" id="password" onChange={(e) => (password.current = e.target.value)} />
+                </div>
+                <button type="submit" disabled={loading} className="mt-8 bg-gray-700 rounded font-bold text-sm text-white w-[85%] h-10 border-none transition-colors duration-150 hover:bg-gray-600 focus:ring-2 focus:ring-blue-200">{loading ? <div className="border-t-2 border-transparent w-7 h-7 border-l-blue-500 rounded-full animate-spin m-auto"></div> : "Login"}</button>
+            </form>
+            {error && <p className="mt-4 text-red-500 font-medium text-lg">{error}</p>}
         </div>
+
     );
     }
 
