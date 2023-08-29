db18b19a42d8b0389f0a1a1d63dfebad4dd139ee
EricJamesCrow <EricCrow@pm.me>
2023-08-29 14:41:51 -0700

    finished contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 import {Input} from "@nextui-org/react";
+import {Textarea} from "@nextui-org/react";
 import localFont from 'next/font/local';
 const railwayThin = localFont({ src: '../../../public/fonts/Raleway-Thin.ttf' });
 
 const Contact: React.FC = () => {
     return (
-        <div className="pb-12 min-h-[91.9vh]">
+        <div className="pb-12 min-h-[70vh] dark text-foreground">
             <h1 className={`${railwayThin.className} mt-4 text-center font-thin text-white text-5xl lg:text-left lg:ml-10 lg:text-7xl`}>Contact</h1>
-            <div className="m-4 mx-10 flex flex-col gap-4 text-white">
-                <Input type="name" variant="underlined" label="Name" placeholder="Enter your name" />
-                <Input type="email" variant="underlined" label="Email" placeholder="Enter your email" />
-                <Input type="subject" variant="underlined" label="Subject" placeholder="Enter your subject" />
-            </div>
+            <form className="mx-auto m-4 px-10 flex flex-col gap-4 text-white max-w-[700px] items-center lg:mt-8">
+                <Input 
+                isRequired
+                type="name" 
+                label="Name" 
+                variant="underlined"
+                placeholder="Enter your name"
+                />
+                <Input isRequired type="email" label="Email" variant="underlined" placeholder="Enter your email" />
+                <Input isRequired type="subject" label="Subject" variant="underlined" placeholder="Enter your subject" />
+                <Textarea
+                isRequired
+                label="Description"
+                variant="underlined"
+                placeholder="Enter your description"
+                />
+                <button type="submit" className="bg-white text-black font-semibold text-sm h-9 rounded mt-2 hover:bg-gray-300 transition-all duration-200 w-full max-w-[320px]">Send Email</button>
+            </form>
         </div>
     )
 }
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -8,7 +8,7 @@ import Card from '@/components/services/card';
 
 const Services: React.FC = () => {
     return (
-        <div className="bg-custom-color pb-12 min-h-[91.9vh]">
+        <div className="bg-custom-color pb-12 min-h-[70vh]">
             <h1 className={`${railwayThin.className} mt-4 text-center font-thin text-white text-5xl lg:text-left lg:ml-10 lg:text-7xl`}>Services</h1>
             <div className="mt-8 m-4 flex flex-col gap-12 lg:m-12">
                 {services.map((service) => (
