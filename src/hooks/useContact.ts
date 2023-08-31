7b8b500e8c921c483f126de71a6da7f5c021fabe
EricJamesCrow <EricCrow@pm.me>
2023-08-31 13:30:57 -0700

    created hook for sending email

---
+++ src/hooks/useContact.ts
@@ -0,0 +1,19 @@
+export const useContact = () => {
+
+    const sendEmail = async (name: string, email: string, subject: string, message: string) => {
+        const res = await fetch('/api/send', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, subject, message }),
+        })
+
+        if (!res.ok) {
+            console.error('Email failed to send')
+            return null
+        }
+
+        const data = await res.json()
+        return data
+    }
+
+    return { sendEmail };
+};
