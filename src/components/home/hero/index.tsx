ac9f2ce95ce477240d556a1bbaabf742dd735a30
EricJamesCrow <EricCrow@pm.me>
2023-09-01 17:00:27 -0700

    added link to button hero section

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import localFont from 'next/font/local'
 
 const railwayThin = localFont({ src: '../../../../public/fonts/Raleway-Thin.ttf' })
@@ -10,21 +11,23 @@ const Hero: React.FC = () => {
                 <span className="block lg:inline-block">Crow</span>Development
                 </h1>
                 <h2 className={`text-[24px] mt-4 mb-8 font-thin lg:text-[36px]`}>Custom-built web applications for your business</h2>
-                <button className="h-11 
-                w-full 
-                rounded-md 
-                border 
-                border-white 
-                bg-custom-color 
-                text-[16px] 
-                hover:bg-gray-700 
-                transition-colors 
-                duration-200 
-                cursor-pointer
-                max-w-[350px]
-                lg:text-[20px]
-                lg:h-[60px]
-                ">Request a quote</button>
+                <Link href="/contact">
+                    <button className="h-11 
+                    w-full 
+                    rounded-md 
+                    border 
+                    border-white 
+                    bg-custom-color 
+                    text-[16px] 
+                    hover:bg-gray-700 
+                    transition-colors 
+                    duration-200 
+                    cursor-pointer
+                    max-w-[350px]
+                    lg:text-[20px]
+                    lg:h-[60px]
+                    ">Request a quote</button>
+                </Link>
             </div>
         </div>
     )
