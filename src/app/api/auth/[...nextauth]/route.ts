b32c93c190f01a4196d2d677af6fbf3266a54d2e
EricJamesCrow <EricCrow@pm.me>
2023-09-03 15:19:26 -0700

    added try catch block

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,58 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
-          // The name to display on the sign in form (e.g. "Sign in with...")
-          name: "Credentials",
-          // `credentials` is used to generate a form on the sign in page.
-          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-          // e.g. domain, username, password, 2FA token, etc.
-          // You can pass any HTML attribute to the <input> tag through the object.
-          credentials: {
-            username: { label: "Username", type: "text" },
-            password: { label: "Password", type: "password" }
-          },
-          async authorize(credentials, req) {
-            // Add logic here to look up the user from the credentials supplied
-            const res = await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
-              method: "POST",
-              body: JSON.stringify({
-                username: credentials?.username,
-                password: credentials?.password
-              }),
-              headers: { "Content-Type": "application/json" }
-            })
+            name: "Credentials",
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                try {
+                    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
+                        method: "POST",
+                        body: JSON.stringify({
+                            username: credentials?.username,
+                            password: credentials?.password
+                        }),
+                        headers: { "Content-Type": "application/json" }
+                    });
 
-            const user = await res.json()
-      
-            if (res.ok) {
-              // Any object returned will be saved in `user` property of the JWT
-              return user
-            } else {
-              // If you return null then an error will be displayed advising the user to check their details.
-              throw new Error(user.error || "Invalid username or password");
-      
-              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                    const user = await res.json();
+
+                    if (res.ok) {
+                        return user;
+                    } else {
+                        throw new Error(user.error || "Invalid username or password");
+                    }
+                } catch (error) {
+                    console.error("Error during authorization:", error);
+
+                    // Type guard to check if error is an instance of Error
+                    if (error instanceof Error) {
+                        throw new Error(error.message || "An error occurred during authorization.");
+                    } else {
+                        throw new Error("An unexpected error occurred during authorization.");
+                    }
+                }
             }
-          }
         })
-      ],
+    ],
     callbacks: {
-      async jwt({token, user}) {
-        return {...token, ...user} ;
-      },
+        async jwt({ token, user }) {
+            return { ...token, ...user };
+        },
 
-      async session({session, token}) {
-        session.user = token as any;
-        return session; 
-      }
+        async session({ session, token }) {
+            session.user = token as any;
+            return session;
+        }
     },
     pages: {
         signIn: '/login',
     },
 });
 
-export {handler as GET, handler as POST};
+export { handler as GET, handler as POST };
