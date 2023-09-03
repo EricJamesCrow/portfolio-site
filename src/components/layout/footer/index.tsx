51e28ed9dbb8a0a68e75384769ad820db6f1b631
EricJamesCrow <EricCrow@pm.me>
2023-09-03 15:28:28 -0700

    footer doesn't show in admin

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,6 +1,14 @@
+"use client";
 import { PiGithubLogo, PiLinkedinLogo } from 'react-icons/pi';
+import { usePathname } from 'next/navigation'
 
 const Footer: React.FC = () => {
+    const pathname = usePathname()
+
+    if (pathname.startsWith('/admin')) {
+        return null;
+    }
+    
     return (
         <div className="relative bg-custom-color text-white w-full min-h-36 flex flex-col items-center py-12">
             <div className="flex gap-3 mb-4">
