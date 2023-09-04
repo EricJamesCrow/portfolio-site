7d08318aabf4ac3a00050fc55164378b68c69538
EricJamesCrow <EricCrow@pm.me>
2023-09-04 12:51:55 -0700

    added more tech

--- src/components/admin/projects/form/tech.tsx
+++ src/components/admin/projects/form/tech.tsx
@@ -55,6 +55,9 @@ const Tech: React.FC<Props> = ({ selectedKeys, setSelectedKeys, setTech, loading
                     <DropdownItem key="C++">C++</DropdownItem>
                     <DropdownItem key="Java">Java</DropdownItem>
                     <DropdownItem key="MySQL">MySQL</DropdownItem>
+                    <DropdownItem key="Postgres">Postgres</DropdownItem>
+                    <DropdownItem key="Prisma">Prisma</DropdownItem>
+                    <DropdownItem key="TailwindCSS">TailwindCSS</DropdownItem>
                 </DropdownMenu>
             </Dropdown>
     </div>
--- src/lib/tech.ts
+++ src/lib/tech.ts
@@ -16,92 +16,41 @@ import Django from '@/public/django.svg';
 import Cplusplus from '@/public/c++.svg';
 import Java from '@/public/java.svg';
 import MySQL from '@/public/mysql.svg';
+import TailwindCSS from '@/public/tailwindcss.svg';
+import Prisma from '@/public/prisma.svg';
+import Postgres from '@/public/postgres.svg';
 
 
 export const technologies = [
-    {
-        id: 1,
-        name: 'React',
-        image: React,
-    },
-    {
-        id: 2,
-        name: 'Redux',
-        image: Redux,
-    },
-    {
-        id: 3,
-        name: 'NextJS',
-        image: Next,
-    },
-    {
-        id: 4,
-        name: 'AWS',
-        image: AWS,
-    },
-    {
-        id: 5,
-        name: 'Vercel',
-        image: Vercel,
-    },
-    {
-        id: 6,
-        name: 'TypeScript',
-        image: TypeScript,
-    },
-    {
-        id: 7,
-        name: 'JavaScript',
-        image: JavaScript,
-    },
-    {
-        id: 8,
-        name: 'Express',
-        image: ExpressJS,
-    },
-    {
-        id: 9,
-        name: 'NodeJS',
-        image: NodeJS,
-    },
-    {
-        id: 10,
-        name: 'MongoDB',
-        image: MongoDB,
-    },
-    {
-        id: 11,
-        name: 'Python',
-        image: Python,
-    },
-    {
-        id: 12,
-        name: 'Qt',
-        image: Qt,
-    },
-    {
-        id: 13,
-        name: 'Electron',
-        image: Electron,
-    },
-    {
-        id: 14,
-        name: 'Django',
-        image: Django,
-    },
-    {
-        id: 15,
-        name: 'C++',
-        image: Cplusplus,
-    },
-    {
-        id: 16,
-        name: 'Java',
-        image: Java,
-    },
-    {
-        id: 17,
-        name: 'MySQL',
-        image: MySQL,
-    },
-]
+    // Frontend Technologies
+    { id: 1, name: 'React', image: React },
+    { id: 2, name: 'Redux', image: Redux },
+    { id: 3, name: 'NextJS', image: Next },
+    { id: 18, name: 'TailwindCSS', image: TailwindCSS },
+
+    // Backend Technologies
+    { id: 9, name: 'NodeJS', image: NodeJS },
+    { id: 8, name: 'Express', image: ExpressJS },
+    { id: 14, name: 'Django', image: Django },
+
+    // Databases
+    { id: 10, name: 'MongoDB', image: MongoDB },
+    { id: 17, name: 'MySQL', image: MySQL },
+    { id: 20, name: 'Postgres', image: Postgres },
+    { id: 19, name: 'Prisma', image: Prisma },
+
+    // Deployment & Hosting
+    { id: 4, name: 'AWS', image: AWS },
+    { id: 5, name: 'Vercel', image: Vercel },
+
+    // Other Frameworks & Libraries
+    { id: 13, name: 'Electron', image: Electron },
+    { id: 12, name: 'Qt', image: Qt },
+
+    // Core Languages
+    { id: 7, name: 'JavaScript', image: JavaScript },
+    { id: 6, name: 'TypeScript', image: TypeScript },
+    { id: 11, name: 'Python', image: Python },
+    { id: 15, name: 'C++', image: Cplusplus },
+    { id: 16, name: 'Java', image: Java },
+];
