7434a762be79f677dbb10ca3e4b70e803481c442
EricJamesCrow <EricCrow@pm.me>
2023-09-04 20:58:24 -0700

    Update page.tsx

--- src/app/admin/projects/page.tsx
+++ src/app/admin/projects/page.tsx
@@ -16,7 +16,7 @@ const Projects: React.FC = () => {
         <div className="w-[95%] grid grid-cols-1 md:grid-cols-2 gap-4 dark text-foreground bg-[#161616] mt-2">
             <div className="text-right w-full">
                 <div className="max-w-[591.73px] mx-auto">
-                    <button onClick={onOpen} className="bg-white text-black font-semibold text-sm h-9 rounded w-36 mb-2 lg:mb-6 hover:bg-gray-300 transition-all duration-200 mr-2 sm:mr-0">Add Project</button>
+                    <button onClick={onOpen} className="bg-white text-black font-semibold text-sm h-9 rounded w-36 mb-2 lg:mb-6 hover:bg-gray-300 transition-all duration-200 mr-5 sm:mr-0">Add Project</button>
                     <DataTable projects={projects} setSelectedProject={setSelectedProject}/>
                 </div>
             </div>
