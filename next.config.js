6ec4652bdab12f13bd1eeb567c0a8bd963804ced
EricJamesCrow <EricCrow@pm.me>
2023-09-22 18:15:06 -0700

    made it so nextauth_url is accessible front-end

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ['crowdevelopment.s3.amazonaws.com'],
     },
+    env: {
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    },
   }
   
   module.exports = nextConfig;
